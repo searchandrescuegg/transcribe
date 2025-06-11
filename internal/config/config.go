574ee819e8d6b7b0b693e644b4449bc5d1b56298
Michael Peters <michael@michaelpeterswa.com>
2025-06-10 22:25:12 -0700

    Initial commit

---
+++ internal/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type Config struct {
+	LogLevel string `env:"LOG_LEVEL" envDefault:"error"`
+
+	MetricsEnabled bool `env:"METRICS_ENABLED" envDefault:"true"`
+	MetricsPort    int  `env:"METRICS_PORT" envDefault:"8081"`
+
+	Local bool `env:"LOCAL" envDefault:"false"`
+
+	TracingEnabled    bool    `env:"TRACING_ENABLED" envDefault:"false"`
+	TracingSampleRate float64 `env:"TRACING_SAMPLERATE" envDefault:"0.01"`
+	TracingService    string  `env:"TRACING_SERVICE" envDefault:"katalog-agent"`
+	TracingVersion    string  `env:"TRACING_VERSION"`
+}
+
+func NewConfig() (*Config, error) {
+	var cfg Config
+
+	err := env.Parse(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse config: %w", err)
+	}
+
+	return &cfg, nil
+}
